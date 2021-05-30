5b98da49c373d5bd5706e2c9ed43208f2a679105
Faiz Ainur Rofiq <cavadevop@gmail.com>
2021-05-30 10:41:36 +0700

    update auth mechanism

--- rest/routes/users.js
+++ rest/routes/users.js
@@ -1,54 +1,53 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var upcc = require('../services/upcc')
-var authService = require('../services/auth')
+var upcc = require("../services/upcc");
+var authService = require("../services/auth");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.get('/list', async (req, res, next) => {
+router.get("/list", async (req, res, next) => {
   try {
-   var result =  await upcc.getAllAssets()
-   res.send(result)
+    var result = await upcc.getAllAssets();
+    res.send(result);
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-})
+});
 
-router.get('/get', authService.validateJwt, async (req, res, next) => {
+router.get("/get", authService.validateJwt, async (req, res, next) => {
   try {
-    var result = await upcc.readAsset(req.query.user_uid)
-    res.send(result)
+    var result = await upcc.readAsset(req.query.user_uid);
+    res.send(result);
   } catch (error) {
     res.status(500);
-    res.send(error.message)
+    res.send(error.message);
   }
-})
+});
 
-router.get('/bank/get', authService.introspectToken, async (req, res, next) => {
+router.get("/bank/get", authService.introspectToken, async (req, res, next) => {
   try {
-    var userUid = `${req.query.user_uid}`
-    if (userUid === '' || userUid === "undefined") {
-      res.status(400)
-      res.send('Bad Request')
+    var userUid = `${req.query.user_uid}`;
+    if (userUid === "" || userUid === "undefined") {
+      res.status(400);
+      res.send("Bad Request");
     } else {
-      var result = await upcc.readAsset(userUid)
-      res.send(result)
+      var result = await upcc.readAsset(userUid);
+      res.send(result);
     }
   } catch (error) {
     res.status(500);
-    res.send(error.message)
+    res.send(error.message);
   }
-})
+});
 
-router.post('/register', authService.validateJwt, async (req, res, next) => {
+router.post("/register", authService.validateJwt, async (req, res, next) => {
   try {
-    
     var user = {
-      userUid: `${req.body.user_uid}`, 
-      email: `${req.body.email}`, 
+      userUid: `${req.body.user_uid}`,
+      email: `${req.body.email}`,
       firstName: `${req.body.first_name}`,
       lastName: `${req.body.last_name}`,
       addressLine1: `${req.body.address_line_1}`,
@@ -58,48 +57,47 @@ router.post('/register', authService.validateJwt, async (req, res, next) => {
       postalCode: `${req.body.postal_code}`,
       ttl: `${req.body.ttl}`,
       nik: `${req.body.nik}`,
-      idCard:`${req.body.id_card}`,
-      businessLicense: `${req.body.business_license}`
+      idCard: `${req.body.id_card}`,
+      businessLicense: `${req.body.business_license}`,
     };
 
-    console.log(user)
+    console.log(user);
 
-    await upcc.createAsset(user)
-    res.status(200)
-    res.send('OK')
+    await upcc.createAsset(user);
+    res.status(200);
+    res.send("OK");
   } catch (error) {
     res.status(400);
-    res.send(error)
+    res.send(error);
   }
-})
+});
 
-router.post('/delete', async (req, res) => {
+router.post("/delete", async (req, res) => {
   try {
-    result = await upcc.deleteAsset(req.body.user_uid)
-    res.status(200)
-    res.send(result)
+    result = await upcc.deleteAsset(req.body.user_uid);
+    res.status(200);
+    res.send(result);
   } catch (error) {
-    res.status(400)
-    res.send(error)
+    res.status(400);
+    res.send(error);
   }
-})
+});
 
-router.get('/exist', async (req, res) => {
+router.get("/exist", async (req, res) => {
   try {
-    result = await upcc.isExist(req.query.user_uid)
-    console.log(`${result}`)
-    res.send(result)
+    result = await upcc.isExist(req.query.user_uid);
+    console.log(`${result}`);
+    res.send(result);
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-})
+});
 
-router.post('/update',authService.validateJwt,  async (req, res, next) => {
+router.post("/update", authService.validateJwt, async (req, res, next) => {
   try {
-    
     var user = {
-      userUid: `${req.body.user_uid}`, 
-      email: `${req.body.email}`, 
+      userUid: `${req.body.user_uid}`,
+      email: `${req.body.email}`,
       firstName: `${req.body.first_name}`,
       lastName: `${req.body.last_name}`,
       addressLine1: `${req.body.address_line_1}`,
@@ -109,26 +107,26 @@ router.post('/update',authService.validateJwt,  async (req, res, next) => {
       postalCode: `${req.body.postal_code}`,
       ttl: `${req.body.ttl}`,
       nik: `${req.body.nik}`,
-      idCard:`${req.body.id_card}`,
-      businessLicense: `${req.body.business_license}`
+      idCard: `${req.body.id_card}`,
+      businessLicense: `${req.body.business_license}`,
     };
-    await upcc.updateAsset(user)
-    res.status(200)
-    res.send('OK')
+    await upcc.updateAsset(user);
+    res.status(200);
+    res.send("OK");
   } catch (error) {
     res.status(500);
-    res.send(error)
+    res.send(error);
   }
-})
+});
 
-router.get('/search', async (req, res) => {
-    try {
-        var result = await upcc.readAssetByEmail(req.query.email)
-        res.send(result)
-    } catch (error) {
-        res.status(400)
-        res.send(error)
-    }
-})
+router.get("/search", authService.introspectToken, async (req, res) => {
+  try {
+    var result = await upcc.readAssetByEmail(req.query.email);
+    res.send(result);
+  } catch (error) {
+    res.status(400);
+    res.send(error);
+  }
+});
 
 module.exports = router;
--- rest/services/auth.js
+++ rest/services/auth.js
@@ -1,35 +1,41 @@
-const { Configuration, AdminApi } = require("@ory/hydra-client");
+const { Configuration, AdminApi, PublicApi } = require("@ory/hydra-client");
 var express = require("express");
-const axios = require('axios').default;
+const axios = require("axios").default;
 const fetch = require("node-fetch");
-
 
 const hydraAdmin = new AdminApi(
   new Configuration({
     basePath: "https://hydra.catena.id/admin",
   })
 );
+const hydraClient = new PublicApi(
+  new Configuration({
+    basePath: "https://hydra.catena.id/admin",
+  })
+);
 
 exports.introspectToken = (req, res, next) => {
-    getAuthToken(req, res, () => {
-        if (req.authToken !== null ){
-            hydraAdmin
-                .introspectOAuth2Token(req.authToken)
-                .then((data) => {
-                var isActive = data.data.active;
-                if (isActive) {
-                    next();
-                } else {
-                    res.status(401);
-                    res.send("Unauthorized");
-                }
-                })
-                .catch((error) => res.send(error));
-        } else {
+  getAuthToken(req, res, () => {
+    if (req.authToken !== null) {
+      hydraAdmin
+        .introspectOAuth2Token(req.authToken)
+        .then((data) => {
+          var isActive = data.data.active;
+          if (isActive) {
+            next();
+          } else {
             res.status(401);
             res.send("Unauthorized");
-        }
-    })
+          }
+        })
+        .catch((error) => res.send(error));
+
+      // hydraClient.revokeOAuth2Token
+    } else {
+      res.status(401);
+      res.send("Unauthorized");
+    }
+  });
 };
 
 var getAuthToken = (req, res, next) => {
@@ -45,28 +51,29 @@ var getAuthToken = (req, res, next) => {
 };
 
 exports.validateJwt = (req, res, next) => {
-    getAuthToken(req, res, async () => {
-        if (req.authToken !== null ){
-            var token = 'Bearer ' + req.authToken
-            var url = 'https://api.catena.id/v1/auth/validate_token'
-            fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Authorization': token
-                }
-            }).then((response) => response.json())
-            .then((json) => {
-                if (json.is_valid) {
-                    next()
-                } else {
-                    res.status(401);
-                    res.send("Unauthorized");
-                }
-            })
-            .catch((error) => res.send())
-        } else {
+  getAuthToken(req, res, async () => {
+    if (req.authToken !== null) {
+      var token = "Bearer " + req.authToken;
+      var url = "https://api.catena.id/v1/auth/validate_token";
+      fetch(url, {
+        method: "POST",
+        headers: {
+          Authorization: token,
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          if (json.is_valid) {
+            next();
+          } else {
             res.status(401);
             res.send("Unauthorized");
-        }
-    })
+          }
+        })
+        .catch((error) => res.send());
+    } else {
+      res.status(401);
+      res.send("Unauthorized");
+    }
+  });
 };
